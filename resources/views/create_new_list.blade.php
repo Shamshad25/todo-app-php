fb811697a849aa7986cf52c572ae1285c90cde86
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2023-12-22 10:49:43 +0530

    Create page created

---
+++ resources/views/create_new_list.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        body{
+            margin-top: 50px;
+            font-family: sans-serif;
+            background: black!important;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+
+        }
+        form{
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body class="antialiased">
+    <div class="container">
+        <div class="text-right"><a href="/" class="btn btn-success">Go Back</a></div>
+
+        <form action="save_new_list">
+            <input type="text" placeholder="Enter new list" name="name">
+            <button type="submit">Save</button>
+        </form>
+
+</div>
+</body>
+</html>
